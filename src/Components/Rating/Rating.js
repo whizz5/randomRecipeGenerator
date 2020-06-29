3ce5dfaebd38b35bec6cbecf54109dbf60fdc289
unknown <whizz5@hotmail.co.uk>
2020-06-29 16:12:10 +0100

    fixed logic resulting in incorrect amount of stars

--- src/Components/Rating/Rating.js
+++ src/Components/Rating/Rating.js
@@ -3,24 +3,16 @@ import { Star, StarFill } from "react-bootstrap-icons";
 
 const Rating = (props) => {
   const randomNumber = Math.floor(Math.random() * 5) + 1; //generates random number 1-5
-  console.log("randomNumber: ", randomNumber);
-
   const starArray = [];
 
-  for (let i = 0; i < randomNumber; i++) {
+  for (let i = 0; i <  randomNumber; i++) {
     starArray.push(<StarFill className="text-warning" size={30} />);
   }
 
-  if (starArray.length < 5) {
-    console.log("starArray.length: ", starArray.length);
-
-    for (let i = 0; i < 5 - starArray.length + 1; i++) {
-      starArray.push(<Star className="text-warning" size={30} />);
-    }
+  while (starArray.length < 5) { //adds empty stars to array until there's 5 in total
+    starArray.push(<Star className="text-warning" size={30} />);
   }
-
-  console.log("starArray: ", starArray);
-
+ 
   return <div className="d-flex align-items-center">{starArray}</div>;
 };
 
